9e4a3c23b471b836cf95fd2d819f8f923298fae6
Yannick MATH <yannick_math@yahoo.fr>
2024-09-01 18:29:25 +0200

    add images

--- src/app/components/welcome/cv.tsx
+++ src/app/components/welcome/cv.tsx
@@ -2,6 +2,7 @@ import { FcAssistant } from "react-icons/fc";
 import useWelcome from "@/app/hooks/useWelcome";
 import CardContainer from "../shared/cardContainer";
 import Button from "../shared/button";
+import Image from "next/image"; // Import the Image component from the correct package
 
 const Cv = () => {
   const { arrayBuilderCvItems } = useWelcome();
@@ -17,8 +18,20 @@ const Cv = () => {
     <CardContainer width="w-1/2 phone:w-3/4 phone:text-center">
       <div className="flex flex-col gap-12 phone:gap-6 p-6 phone:p-2 items-center">
         <div className="flex flex-row phone:flex-col h-1/4">
-          <div className="flex justify-center items-center w-1/3 phone:w-full">
-            <FcAssistant className="w-20 h-20" />
+          <div className="flex justify-center items-center w-1/3 phone:w-full ">
+            {/* <FcAssistant className="w-20 h-20" /> */}
+            <div className="phone:h-64 phone:w-36 relative">
+              <Image
+                src="/cecile2.png"
+                // width={200}
+                // height={150}
+                layout="fill"
+                objectFit="cover"
+                alt="Cécile Boiron"
+                className="rounded-lg"
+              />
+            </div>
+            {/* Use the Image component */}
           </div>
           <div className="flex flex-col items-start justify-center rounded-xl text-lg phone:text-sm w-2/3 phone:w-full gap-4 p-2">
             <Items />
