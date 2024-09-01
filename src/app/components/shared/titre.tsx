a6fb14627409a31cd784e9c294228c1bc7d17ade
Yannick MATH <yannick_math@yahoo.fr>
2024-09-01 21:13:58 +0200

    update responsive

--- src/app/components/shared/titre.tsx
+++ src/app/components/shared/titre.tsx
@@ -5,18 +5,20 @@ interface TitreProps {
 
 export default function Titre({ title, description }: TitreProps): JSX.Element {
   return (
-    <div
-      className=" flex flex-col w-full h-[150px] justify-center items-center p-8 text-center gap-10
-    "
-    >
+    <div className="flex flex-col w-full h-[150px] justify-center items-center p-8 text-center gap-10">
       <div
-        className="flex justify-center items-center 
-                  shadow-xl border-b border-r border-black p-4 bg-opacity-30
-                  phone:bg-office-manager-main-bg phone:bg-cover phone:bg-no-repeat phone:bg-center phone:bg-opacity-20"
+        className="relative flex justify-center items-center 
+                    shadow-xl border-b border-r border-black p-4 
+                    phone:bg-office-manager-main-bg phone:bg-cover phone:bg-no-repeat phone:bg-center"
       >
-        <div className="flex flex-col justify-center gap-8 phone:gap-4">
-          <h1 className="text-3xl phone:text-xl font-bold">{title}</h1>
-          <p className="text-x phone:text-base phone:font-bolder">
+        {/* Overlay semi-transparent */}
+        <div className="absolute inset-0 bg-black opacity-20 phone:opacity-50"></div>
+
+        <div className="relative flex flex-col justify-center gap-8 phone:gap-4">
+          <h1 className="text-3xl phone:text-xl font-bold text-white">
+            {title}
+          </h1>
+          <p className="text-xl phone:text-base phone:font-bolder text-white">
             {description}
           </p>
         </div>
