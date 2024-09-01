d82ad62569b0a79349371c7806d5c5e229294670
Yannick MATH <yannick_math@yahoo.fr>
2024-09-01 17:47:14 +0200

    add css for test following prod test on vercel

--- src/app/components/welcome/cv.tsx
+++ src/app/components/welcome/cv.tsx
@@ -15,7 +15,7 @@ const Cv = () => {
 
   return (
     <CardContainer width="w-1/2 phone:w-3/4 phone:text-center">
-      <div className="flex flex-col gap-12 p-6 items-center">
+      <div className="flex flex-col gap-12 phone:gap-6 p-6 items-center">
         <div className="flex flex-row phone:flex-col h-1/4">
           <div className="flex justify-center items-center w-1/3 phone:w-full">
             <FcAssistant className="w-20 h-20" />
